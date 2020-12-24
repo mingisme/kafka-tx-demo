20056476ee715f1e4629f1907e8fbfd48aabde0c
ming.wang4 <ming.wang4@l-008645.local>
2020-12-24 10:54:04 +0800

    init

---
+++ src/main/java/com/example/sprintbootkafkatxdemo/SprintbootKafkaTxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootkafkatxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootKafkaTxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootKafkaTxDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintbootkafkatxdemo/SprintbootKafkaTxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootkafkatxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintbootKafkaTxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
