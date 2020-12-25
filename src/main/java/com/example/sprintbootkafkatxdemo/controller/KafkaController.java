11d4ff55473429f11fd485a601f7e0b75cf98c7d
ming.wang4 <ming.wang4@l-008645.local>
2020-12-25 17:03:06 +0800

    transaction workable

--- src/main/java/com/example/sprintbootkafkatxdemo/controller/KafkaController.java
+++ src/main/java/com/example/sprintbootkafkatxdemo/controller/KafkaController.java
@@ -3,8 +3,11 @@ package com.example.sprintbootkafkatxdemo.controller;
 import com.example.sprintbootkafkatxdemo.service.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(value = "/kafka")
@@ -28,8 +31,13 @@ public class KafkaController {
     @PostMapping(path = "/send/foos/{what}")
     public void sendFoo(@PathVariable String what) {
         this.kafkaTemplate.executeInTransaction(kafkaTemplate -> {
-            StringUtils.commaDelimitedListToSet(what).stream()
-                    .forEach(foo -> kafkaTemplate.send("topic2", foo));
+            Set<String> foos = StringUtils.commaDelimitedListToSet(what);
+            for(String foo : foos){
+                if(foo.equals("e")){
+                    throw new RuntimeException("fail");
+                }
+                kafkaTemplate.send("topic2", foo);
+            }
             return null;
         });
     }
